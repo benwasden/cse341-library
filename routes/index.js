81c78fc49510117ecd17181f7dcb76c5d3397158
benwasden <was24001@byui.edu>
2025-10-01 07:46:25 -0700

    Working basic routes

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const passport = require('passport');
+
+router.use('/', require('./swagger'));
+
+router.use('/books', require('./books'));
+
+router.use('/reviews', require('./reviews'));
+
+router.use('/users', require('./users'));
+
+router.use('/groups', require('./groups'));
+
+router.get('/login', passport.authenticate('github'), (req, res) => {});
+
+router.get('/logout', function(req, res, next) {
+    req.logout(function(err) {
+        if (err) { return next(err); }
+        res.redirect('/');
+    });
+});
+
+module.exports = router;
---
+++ swagger.js
@@ -0,0 +1,15 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+    info: {
+        title: 'Library API',
+        description: 'CSE341 Library Project'
+    },
+    host: 'localhost:3000',
+    schemes: ['https']
+};
+
+const outputFile = './swagger.json';
+const endpointsFiles = ['./routes/index.js'];
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
